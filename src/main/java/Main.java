fd8f0627f526299f60ec0b9cd8c567c982329761
frwrks <vennnq@gmail.com>
2017-02-26 12:49:18 +0100

    Created Main file, edited NeuralNetwork.class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,36 @@
+import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
+import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
+
+import java.io.IOException;
+
+
 /**
  * Created by Volker on 26.02.17.
  */
 public class Main {
 
-    public static void main(String[] args) {
-        
+//    private static Logger log = LoggerFactory.getLogger(Main.class);
+
+
+    public static void main(String[] args) throws IOException {
+
+//        NeuralNetwork neuralNetwork = new NeuralNetwork();
+        MultiLayerConfiguration conf = NeuralNetwork.configNeuralNetwork();
+
+        MultiLayerNetwork model = new MultiLayerNetwork(conf);
+        model.init();
+//        ImagePipeline2 imagePipeline2 = new ImagePipeline2();
+        DataSetIterator dataIter = ImagePipeline2.createDataIter();
+
+        model.setListeners(new ScoreIterationListener(10));
+//        log.info("******TRAIN MODEL*******");
+
+        for (int i = 0; i < Configuration.numEpochs; i++) {
+            model.fit(dataIter);
+
+
+        }
     }
 }
