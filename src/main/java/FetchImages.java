d8faab671f8b964a80dc719a5bf46eaed6888462
frwrks <vennnq@gmail.com>
2017-03-10 20:00:28 +0100

    Added class FetchImages

---
+++ src/main/java/FetchImages.java
@@ -0,0 +1,21 @@
+import java.awt.*;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by Volker on 10.03.17.
+ */
+public class FetchImages {
+
+
+
+    Image image = null;
+    public static void fetchImages() throws MalformedURLException {
+        try {
+            URL url = new URL("http://www.google.de");
+        } catch ( IOException e ) {}
+
+
+    }
+}
