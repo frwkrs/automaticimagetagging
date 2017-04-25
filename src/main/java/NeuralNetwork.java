e95bb5fa10acd3efe87e4dc6f8602648680c3a7e
frwrks <vennnq@gmail.com>
2017-04-25 14:04:56 +0200

    deleted configurations class, added configuration to neural networks class

--- src/main/java/NeuralNetwork.java
+++ src/main/java/NeuralNetwork.java
@@ -1,49 +1,122 @@
 import org.deeplearning4j.nn.api.OptimizationAlgorithm;
-import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
-import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
-import org.deeplearning4j.nn.conf.Updater;
+import org.deeplearning4j.nn.conf.*;
+import org.deeplearning4j.nn.conf.distribution.Distribution;
+import org.deeplearning4j.nn.conf.distribution.GaussianDistribution;
+import org.deeplearning4j.nn.conf.distribution.NormalDistribution;
 import org.deeplearning4j.nn.conf.inputs.InputType;
-import org.deeplearning4j.nn.conf.layers.DenseLayer;
-import org.deeplearning4j.nn.conf.layers.OutputLayer;
+import org.deeplearning4j.nn.conf.layers.*;
 import org.deeplearning4j.nn.weights.WeightInit;
+import org.nd4j.linalg.activations.Activation;
+import org.nd4j.linalg.lossfunctions.LossFunctions;
+
 
 /**
  * Created by Volker on 25.02.17.
  */
 public class NeuralNetwork {
 
-//    private static Logger log = LoggerFactory.getLogger(NeuralNetwork.class);
+    // size of the images
+    // they are going to be transformed to that size
+    // also size of the matrix
+    private static final int height =1024;
+    private static final int width =768;
+    // channels = red green blue
+    private static final int channels = 3;
+    // seed for repeatable tests
+    private static final int rngseed = 123;
+    private static final int batchSize = 1;
+    // number of possible labels
+//    has to be calculated
+    private static final int outputNum = 2;
 
-    public static MultiLayerConfiguration configNeuralNetwork() {
+    private static final int numEpochs = 15;
+
+    private static final double learningRate = 0.006;
+    private  static final int iterations = 1;
+    private static final double momentum = 0.9;
+
+    private static final double nonZeroBias = 1;
+    //  dropOut ignors some neurons to prevent overfitting the modell to the training dataset
+    private static final double dropOut = 0.5;
+
+
+
+//    private static Logger log = LoggerFactory.getLogger(NeuralNetwork.class);
+private ConvolutionLayer convInit(String name, int in, int out, int[] kernel, int[] stride, int[] pad, double bias) {
+//    in = no of inputs
+//
+    return new ConvolutionLayer.Builder(kernel, stride, pad).name(name).nIn(in).nOut(out).biasInit(bias).build();
+}
+
+    private ConvolutionLayer conv3x3(String name, int out, double bias) {
+        return new ConvolutionLayer.Builder(new int[]{3,3}, new int[] {1,1}, new int[] {1,1}).name(name).nOut(out).biasInit(bias).build();
+    }
+
+    private ConvolutionLayer conv5x5(String name, int out, int[] stride, int[] pad, double bias) {
+        return new ConvolutionLayer.Builder(new int[]{5,5}, stride, pad).name(name).nOut(out).biasInit(bias).build();
+    }
+
+    private SubsamplingLayer maxPool(String name,  int[] kernel) {
+        return new SubsamplingLayer.Builder(kernel, new int[]{2,2}).name(name).build();
+    }
+
+    private DenseLayer fullyConnected(String name, int out, double bias, double dropOut, Distribution dist) {
+        return new DenseLayer.Builder().name(name).nOut(out).biasInit(bias).dropOut(dropOut).dist(dist).build();
+    }
+
+    public MultiLayerConfiguration configNeuralNetwork() {
+
+
 
 
 //        log.info("**** CREATING NEURAL NETWORK");
         MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
-                .seed(Configuration.rngseed)
+                .seed(rngseed)
+                .weightInit(WeightInit.DISTRIBUTION)
+                .dist(new NormalDistribution(0.0, 0.01))
+                .activation(Activation.RELU)
+                .updater(Updater.NESTEROVS)
+                .iterations(iterations)
+                .gradientNormalization(GradientNormalization.RenormalizeL2PerLayer) // normalize to prevent vanishing or exploding gradients
                 .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
-                .iterations(Configuration.iterations)
-                .learningRate(Configuration.learningRate)
-                .updater(Updater.NESTEROVS).momentum(Configuration.momentum)
-                .regularization(true).l2(1e-4)
+                .learningRate(1e-2)
+                .biasLearningRate(1e-2 * 2)
+                .learningRateDecayPolicy(LearningRatePolicy.Step)
+                .lrPolicyDecayRate(0.1)
+                .lrPolicySteps(100000)
+                .regularization(true)
+                .l2(5 * 1e-4)
+                .momentum(0.9)
+                .miniBatch(false)
                 .list()
-                .layer(0, new DenseLayer.Builder()
-                        .nIn(Configuration.height * Configuration.width)
-                        .nOut(100)
-//                        .activation("relu")
-                        .weightInit(WeightInit.XAVIER)
+                .layer(0, convInit("cnn1", channels, 96, new int[]{11, 11}, new int[]{4, 4}, new int[]{3, 3}, 0))
+                .layer(1, new LocalResponseNormalization.Builder().name("lrn1").build())
+                .layer(2, maxPool("maxpool1", new int[]{3, 3}))
+                .layer(3, conv5x5("cnn2", 256, new int[]{1, 1}, new int[]{2, 2}, nonZeroBias))
+                .layer(4, new LocalResponseNormalization.Builder().name("lrn2").build())
+                .layer(5, maxPool("maxpool2", new int[]{3, 3}))
+                .layer(6, conv3x3("cnn3", 384, 0))
+                .layer(7, conv3x3("cnn4", 384, nonZeroBias))
+                .layer(8, conv3x3("cnn5", 256, nonZeroBias))
+                .layer(9, maxPool("maxpool3", new int[]{3, 3}))
+                .layer(10, fullyConnected("ffn1", 4096, nonZeroBias, dropOut, new GaussianDistribution(0, 0.005)))
+                .layer(11, fullyConnected("ffn2", 4096, nonZeroBias, dropOut, new GaussianDistribution(0, 0.005)))
+                .layer(12, new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
+                        .name("output")
+                        .nOut(outputNum)
+                        .activation(Activation.SOFTMAX)
                         .build())
-                .layer(1, new OutputLayer.Builder()
-                        .nIn(100)
-                        .nOut(Configuration.outputNum)
-//                        .activation("softmax")
-                        .weightInit(WeightInit.XAVIER)
-                        .build())
-                .pretrain(false).backprop(true)
-                .setInputType(InputType.convolutional(Configuration.height, Configuration.width, Configuration.channels))
+                .backprop(true)
+                .pretrain(false)
+                .setInputType(InputType.convolutional(height, width, channels))
                 .build();
 
         return conf;
 
     }
-}
+
+
+
+    }
+
 
